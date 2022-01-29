cd51fb539e18d03810ff637c998cff6f9f0a1641
Vipershroom <undertalefan812@gmail.com>
2022-01-29 15:09:16 -0500

    Added comments

--- mrhelp.py
+++ mrhelp.py
@@ -10,12 +10,14 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 testing_servers = [684237072827154435]
 
+# Welcome message for when the bot comes online      
 @bot.event
 async def on_ready():
     print(f"{bot.user} Has logged on")
     for server in bot.guilds:
         print(server)
-        
+  
+# Filters out messages that don't start with the $ sign
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -23,26 +25,28 @@ async def on_message(message):
     elif message.content[0] != "$":
         return
     
+    # Initializes the user who sent the message along with grabbing message content
     username = str(message.author).split("#")[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f"{username}: {user_message} ({channel})")
     
+    # randomm commands
     if user_message.lower() == "$random":
         response = f"This is your random number, {random.randint(1, 1000)}"
         await message.channel.send(response)
         return
+    # hello command
     elif user_message.lower() == "$hello": 
         await message.channel.send(f"Hello, {username}")
         return
 
-
+# slash commands
 @bot.slash_command(guild_ids=testing_servers)
 async def hello(ctx):
     await ctx.respond("Hello!")
     return
-
-    
+ 
 @bot.slash_command(guild_ids=testing_servers)
 async def no(ctx):
     await ctx.respond("No, lol")
