2c85880cc18b97a73070df0f49682b646d4cd4f7
Vipershroom <undertalefan812@gmail.com>
2022-01-29 23:25:05 -0500

    Set $ prefix to a variable

--- mrhelp.py
+++ mrhelp.py
@@ -9,6 +9,7 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 testing_servers = [684237072827154435, 920013664604553246]
+prefix = "$"
 
 # Welcome message for when the bot comes online      
 @bot.event
@@ -35,7 +36,7 @@ async def on_message(message):
     print(f"{username}: {user_message} ({channel})")
     
     #help command
-    if user_message.lower() == "$help":
+    if user_message.lower() == f"{prefix}help":
         response = """
 ```
 $help Displays all commands
@@ -47,16 +48,16 @@ $echo Echo
         await message.channel.send(response)
         return
     # random command
-    elif user_message.lower() == "$random":
+    elif user_message.lower() == f"{prefix}random":
         response = f"This is your random number, {random.randint(1, 1000)}"
         await message.channel.send(response)
         return
     # hello command
-    elif user_message.lower() == "$hello": 
+    elif user_message.lower() == f"{prefix}hello": 
         await message.channel.send(f"Hello, {username}")
         return
     # echo command
-    elif user_message.startswith("$echo"):
+    elif user_message.startswith(f"{prefix}echo"):
         echoc = user_message.replace("$echo", "")
         await message.channel.send(f"**echo**{echoc}")
         return
