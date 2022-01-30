243bcf2bf178de25f30e906e6ab38dd8c3bf60f8
Vipershroom <undertalefan812@gmail.com>
2022-01-30 14:42:56 -0500

    Added slash command for 8ball

--- mrhelp.py
+++ mrhelp.py
@@ -52,11 +52,11 @@ $echo Echo
     await ctx.send(response)
     
 @bot.command()
-async def echo(ctx, *arg):
-    if arg == ():
+async def echo(ctx, *message):
+    if message == ():
         await ctx.send("Give me something")
     else:
-        message = str(arg)[2:-3]
+        message = str(message)[2:-3]
         await ctx.send(message)
     
     
@@ -71,8 +71,8 @@ async def hello(ctx):
     await ctx.send("Hello!")
     
 @bot.command(aliases=['8ball'])
-async def _8ball(ctx, *arg):
-    if arg == ():
+async def _8ball(ctx, *message):
+    if message == ():
         await ctx.send("8ball needs a question!")
     else:
         # 8ball responses
@@ -129,7 +129,38 @@ $echo Echo
     await ctx.respond(response)
 
 @bot.slash_command(guild_ids=testing_servers, name="echo", description="Echos the displayed message")
-async def echo(ctx, arg):
-    await ctx.respond(arg)
+async def echo(ctx, message):
+    await ctx.respond(message)
+    
+@bot.slash_command(guild_ids=testing_servers, name="8ball", description="Ask the 8ball anything")
+async def eightball(ctx, message):
+        # 8ball responses
+        msg = message
+        responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+            ]
+        # picks a random response from the list and sends to user
+        random.shuffle(responses)
+        randomitem = responses[random.randrange(0, len(responses))]
+        await ctx.respond(f"(Original message: {msg}) {randomitem}")
     
 bot.run(TOKEN)
